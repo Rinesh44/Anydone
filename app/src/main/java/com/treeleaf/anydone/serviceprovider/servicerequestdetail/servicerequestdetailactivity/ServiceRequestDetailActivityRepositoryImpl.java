3e78eebc1c7294feef5e01b612b3e683827ab390
Jenish Shakya <jenishshakya33@gmail.com>
2020-10-12 16:24:11 +0545

    fix issue of janus url not being fetched

--- app/src/main/java/com/treeleaf/anydone/serviceprovider/servicerequestdetail/servicerequestdetailactivity/ServiceRequestDetailActivityRepositoryImpl.java
+++ app/src/main/java/com/treeleaf/anydone/serviceprovider/servicerequestdetail/servicerequestdetailactivity/ServiceRequestDetailActivityRepositoryImpl.java
@@ -3,15 +3,18 @@ package com.treeleaf.anydone.serviceprovider.servicerequestdetail.servicerequest
 
 import com.treeleaf.anydone.serviceprovider.rest.service.AnyDoneService;
 import com.treeleaf.anydone.rpc.RtcServiceRpcProto;
+import com.treeleaf.anydone.serviceprovider.utils.GlobalUtils;
 
 import io.reactivex.Observable;
+import retrofit2.Retrofit;
 
 public class ServiceRequestDetailActivityRepositoryImpl implements ServiceRequestDetailActivityRepository {
 
     AnyDoneService service;
 
-    public ServiceRequestDetailActivityRepositoryImpl(AnyDoneService service) {
-        this.service = service;
+    public ServiceRequestDetailActivityRepositoryImpl(AnyDoneService service1) {
+        Retrofit retrofit = GlobalUtils.getRetrofitInstance();
+        service = retrofit.create(AnyDoneService.class);
     }
 
     @Override
