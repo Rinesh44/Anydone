e8de10b57296e88dbd4df83ed520f40f07977363
Jenish Shakya <jenishshakya33@gmail.com>
2021-02-03 13:42:12 +0545

    show mqtt log

--- app/src/main/java/com/treeleaf/anydone/serviceprovider/ticketdetails/ticketconversation/TicketConversationPresenterImpl.java
+++ app/src/main/java/com/treeleaf/anydone/serviceprovider/ticketdetails/ticketconversation/TicketConversationPresenterImpl.java
@@ -1897,7 +1897,7 @@ public class TicketConversationPresenterImpl extends BasePresenter<TicketConvers
     }
 
     public void sendMqttLog(String eventName, boolean ownResponse) {
-        if (false)
+        if (true)
             getView().onMqttResponseReceivedChecked(eventName, ownResponse);
     }
 
