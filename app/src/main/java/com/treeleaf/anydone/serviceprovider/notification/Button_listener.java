9535cc9fe4e23e10e90841b0f3bb906dfbf1afb0
Jenish Shakya <jenishshakya33@gmail.com>
2021-03-11 13:16:12 +0545

    test

---
+++ app/src/main/java/com/treeleaf/anydone/serviceprovider/notification/Button_listener.java
@@ -0,0 +1,22 @@
+package com.treeleaf.anydone.serviceprovider.notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class Button_listener extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+//        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+//        manager.cancel(intent.getExtras().getInt("id"));
+//        Toast.makeText(context, "Button clicked!", Toast.LENGTH_SHORT).show();
+
+
+        String message = intent.getStringExtra("id");
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+
+    }
+}
