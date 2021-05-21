7ccd079494d35f73240b5ce0a4d2c699c1c78b7f
Jenish Shakya <jenishshakya33@gmail.com>
2021-05-21 10:16:44 +0545

    update video call library

---
+++ treeleaf-webrtc-library/src/main/java/com/treeleaf/januswebrtc/PermissionHandlerActivity.java
@@ -0,0 +1,149 @@
+package com.treeleaf.januswebrtc;
+
+
+import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+
+public abstract class PermissionHandlerActivity extends AppCompatActivity implements SensorEventListener {
+
+    public static final String TAG = PermissionHandlerActivity.class.getSimpleName();
+    public static final int REQUEST_CODE = 100;
+    public String[] neededPermissions = new String[]{CAMERA, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE, RECORD_AUDIO};
+    public Boolean permissionsGranted = false;
+
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+
+    private PowerManager powerManager;
+    private PowerManager.WakeLock wakeLock;
+    private int field = 0x00000020;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        try {
+            field = PowerManager.class.getClass().getField("PROXIMITY_SCREEN_OFF_WAKE_LOCK").getInt(null);
+        } catch (Throwable throwable) {
+            Log.d(TAG, throwable.getLocalizedMessage());
+        }
+        powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        wakeLock = powerManager.newWakeLock(field, getLocalClassName());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        WindowManager.LayoutParams params = this.getWindow().getAttributes();
+        float distance = event.values[0];
+        if (distance < 5f) {
+            if (!wakeLock.isHeld()) {
+                wakeLock.acquire();
+            }
+        } else {
+            Log.d(TAG, "proximity >5f: " + distance);
+            if (wakeLock.isHeld()) {
+                wakeLock.release();
+            }
+        }
+    }
+
+    public boolean checkPermission() {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            ArrayList<String> permissionsNotGranted = new ArrayList<>();
+            for (String permission : neededPermissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    permissionsNotGranted.add(permission);
+                }
+            }
+            if (permissionsNotGranted.size() > 0) {
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]));
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private void requestPermissions(String[] permissions) {
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE:
+                for (int result : grantResults) {
+                    if (result == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "Cannot start camera without permission.", Toast.LENGTH_LONG).show();
+                        finish();
+                        return;
+                    }
+                }
+                permissionsGranted = true;
+                onPermissionGranted();
+                break;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    public abstract void onPermissionGranted();
+
+}
+
