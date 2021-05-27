66b4cc2f1f59b75ae52f97e08c1da3157c2a47ef
Jenish Shakya <jenishshakya33@gmail.com>
2021-05-27 20:35:08 +0545

    show message for when selected participants are invited to the call

--- app/src/main/java/com/anydone/desk/videocallreceive/VideoCallReceivePresenterImpl.java
+++ app/src/main/java/com/anydone/desk/videocallreceive/VideoCallReceivePresenterImpl.java
@@ -5,6 +5,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.text.TextUtils;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
@@ -1356,6 +1357,9 @@ public class VideoCallReceivePresenterImpl extends
                                     if (addCallParticipant.getAccountIdsList().contains(userAccountId)) {
 //                                        getView().onVideoRoomInvite(addCallParticipant, relayResponse.getContext());
                                     }
+                                } else if (userAccountId.equals(addCallParticipant.getSenderAccountId())) {
+                                    Toast.makeText(getContext(), "Selected participants invited to this call",
+                                            Toast.LENGTH_LONG).show();
                                 }
                                 sendMqttLog("ADD_CALL_PARTICIPANT", userAccountId.equals(addCallParticipant.getSenderAccountId()));
                             }
