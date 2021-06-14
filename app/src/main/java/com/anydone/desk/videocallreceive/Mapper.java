87dc20108ffd21b43c84bf763073cc2ce5c44711
Jenish Shakya <jenishshakya33@gmail.com>
2021-06-14 12:33:55 +0545

    add method for showing relative time

--- app/src/main/java/com/anydone/desk/videocallreceive/Mapper.java
+++ app/src/main/java/com/anydone/desk/videocallreceive/Mapper.java
@@ -1,7 +1,11 @@
 package com.anydone.desk.videocallreceive;
 
+import com.github.marlonlom.utilities.timeago.TimeAgo;
+import com.github.marlonlom.utilities.timeago.TimeAgoMessages;
 import com.treeleaf.januswebrtc.tickets.model.AssignEmployee;
 import com.treeleaf.januswebrtc.tickets.model.Tickets;
+
+import java.util.Locale;
 
 public class Mapper {
 
@@ -22,6 +26,7 @@ public class Mapper {
         newTicket.setCustomerType(tickets.getCustomerType());
         newTicket.setTicketType(tickets.getTicketType());
         newTicket.setCreatedAt(tickets.getCreatedAt());
+        newTicket.setRelativeTime(getRelativeTime(tickets));
         newTicket.setTicketStatus(tickets.getTicketStatus());
         newTicket.setCreatedByName(tickets.getCreatedByName());
         newTicket.setCreatedByPic(tickets.getCreatedByPic());
@@ -34,15 +39,21 @@ public class Mapper {
 
     public static AssignEmployee transform(com.anydone.desk.realm.model.AssignEmployee assignEmployee) {
         AssignEmployee newEmployee = new AssignEmployee();
-        newEmployee.setEmployeeId(newEmployee.getEmployeeId());
-        newEmployee.setAccountId(newEmployee.getAccountId());
-        newEmployee.setCreatedAt(newEmployee.getCreatedAt());
-        newEmployee.setEmployeeImageUrl(newEmployee.getEmployeeImageUrl());
-        newEmployee.setEmail(newEmployee.getEmail());
-        newEmployee.setPhone(newEmployee.getPhone());
-        newEmployee.setName(newEmployee.getName());
+        newEmployee.setEmployeeId(assignEmployee.getEmployeeId());
+        newEmployee.setAccountId(assignEmployee.getAccountId());
+        newEmployee.setCreatedAt(assignEmployee.getCreatedAt());
+        newEmployee.setEmployeeImageUrl(assignEmployee.getEmployeeImageUrl());
+        newEmployee.setEmail(assignEmployee.getEmail());
+        newEmployee.setPhone(assignEmployee.getPhone());
+        newEmployee.setName(assignEmployee.getName());
         return newEmployee;
     }
 
+    private static String getRelativeTime(com.anydone.desk.realm.model.Tickets tickets) {
+        Locale localeByLanguageTag = Locale.forLanguageTag("np");
+        TimeAgoMessages messages = new TimeAgoMessages.Builder().withLocale(localeByLanguageTag).build();
+        String relativeTime = TimeAgo.using(tickets.getCreatedAt(), messages);
+        return relativeTime;
+    }
 
 }
